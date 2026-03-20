3a6dce81ea8e197144235aeb13a64e9dccfedb3e
Igetsisss <98964802+Igetsisss@users.noreply.github.com>
2026-03-20 11:27:28 -0400

    Refactor grade word lists and update logic

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,33 +1,170 @@
-import { GradeNumber, loadGradeFromLocalStorage, saveGradeToLocalStorage, } from '../lib/localStorage'
+import {
+  GradeNumber,
+  loadGradeFromLocalStorage,
+  saveGradeToLocalStorage,
+} from '../lib/localStorage'
 
 const gradeStatKey = 'gradeNumber'
 const grade = localStorage.getItem(gradeStatKey)
 const nogradeyet = ['error']
-var GRADEWORDS = nogradeyet
 
-const ELEVENTHGRADE = [
-    "emerson", "william", "mitchell", "rhodes", "daniel", "brooke", "avery", "harris", "grant", "madeline", "cesar",
-    "gavin", "davis", "margot", "hotoniel", "jack", "sam", "robby", "margaret", "gigi",
-    "christina", "emmy", "felipe", "florencia", "grey", "claire", "caroline", "hailey", "ryan", "jackson",
-    "aj", "amara", "jon", "anaya", "patrick", "juliana", "nicholas", "finn", "connor", "andrew",
-    "ella", "emma", "kate", "mary", "brendan", "joselyn", "mateo", "eliana", "graham", "nora",
-    "will", "tuck", "amaree", "phillip", "maxx", "betsy", "sophie", "cole", "grant", "alexa",
-    "sammy", "sidney-ann", "blair", "evelyn", "gracie", "ayokunle", "meriwether", "paige", "ria",
-    "amelia", "hannah", "katlynn", "teagan", "beatrice", "sarah", "vance", "hunter", "brad", "tyler",
-    "greg", "franklin", "braiden", "annabelle", "preston", "maddie", "tommy", "collier", "colt",
-    "dean", "havana", "matthew", "lily", "kade", "griffin", "hayes", "mose", "vandy", "logan",
-    "scarlett", "mcvey", "george", "chloe", "luke", "layla", "ryder", "ray", "victoria", "mayomi",
-    "maya", "bryce", "sienna", "bobby", "charlie", "lilah", "sean", "kenzo", "leah", "jules",
-    "gabriella", "charlotte", "sasha", "sophia", "tanner", "marley", "eva", "brent", "chase", "harley",
-    "makayla", "grayson", "claire", "christian", "munchie", "nicole"
+var GRADEWORDS = nogradeyet
+const EIGHTGRADE = [
+  "avery", "ella", "joselyn", "grant", "havana", "sean", "william", "mitchell", "hannah", "cole", "ryan", "hunter", "hayes", "hailey", "scarlett", "blair", "nicole", "moses", "emma", "sophia", "annabelle", "brad", "emerson", "danny", "andrew", "robert", "sidney-ann", "tyson", "lillian", "caroline", "aj", "jack", "cesar", "amara", "ray", "ansley", "sophia", "leah", "katherine", "nakhai", "sienna", "luke", "ria", "caroline", "evelyn", "avalina", "tanner", "gavin", "kaitlynne", "will", "chase", "collier", "franklin", "bobby", "hotoniel", "daniel", "sam", "tuck", "emmy", "finn", "jones", "juliana", "maxx",
+  "lilliana", 
+  "amelia", 
+  "claire", 
+  "jules", 
+  "meriwether", 
+  "elizabeth", 
+  "andrew", 
+  "layla",
+  "greg", 
+  "griffin",
+  "eliana",
+  "emma", 
+  "alexa",
+  "ryder", 
+  "marley",
+  "bryce",
+  "jack", 
+  "mcvey", "brent", "sasha", "jackson", "bralyn", "brooke", "caroline", "eva", "mary", "emma", "felipe", "patrick", "gabriella", "dean", "sarah", "tommy", "margot", "garrett", "margaret", "madeline", "preston", "phillip", "rhodes", "robby", "charlotte", "beatrice", "diego", "christina", "jackson", "eli", "amaree", "george", "kenzo", "tyler", "logan", "nora", "grace", "emma", "brendan", "grayson", "greyson", "katlynn", "madeline", "christian", "davis", "kade", "colt", "claire", "bryce", "matthew", "braiden", "william", "chloe", "teagan", "jack", "betsy", "connor", "jon", "ella", "olivia", "nick", "makayla", "harley", "paige", "grant", "sammy", "grace", "carter", "graham", "jack", "ryder"
 ];
 
-if (grade == null) {
-    var GRADEWORDS = nogradeyet
-} else if (grade == '"11"') {
-    var GRADEWORDS = ELEVENTHGRADE
-} else {
-    var GRADEWORDS = nogradeyet
-}
 
-export const WORDS = GRADEWORDS;
+
+
+const SEVENGRADE = [
+  'presley',
+  'liam',
+  'violet',
+  'sarah',
+  'grayden',
+  'lucy',
+  'cristiano',
+  'alexa',
+  'jake',
+  'lucas',
+  'prakhar',
+  'athena',
+  'ayden',
+  'william',
+  'sloane',
+  'perry',
+  'mia',
+  'tristen',
+  'davis',
+  'crosby',
+  'hugh',
+  'gailyn',
+  'sophia',
+  'jackson',
+  'logan',
+  'cathryn-rose',
+  'brayden',
+  'mary',
+  'isabel',
+  'sam',
+  'jack',
+  'ronan',
+  'isabella',
+  'elizabeth',
+  'lane',
+  'sovereign',
+  'caroline',
+  'samantha',
+  'joshua',
+  'laura',
+  'daniel',
+  'daniel',
+  'emma',
+  'ryan',
+  'ryan',
+  'turner',
+  'wyatt',
+  'connor',
+  'cole',
+  'julia',
+  'zoe',
+  'isabell',
+  'william',
+  'vivienne',
+  'emily',
+  'samuel',
+  'patrick',
+  'sara',
+  'peighton',
+  'jay',
+  'shane',
+  'emerson',
+  'hudson',
+  'colton',
+  'caroline',
+  'ryan',
+  'riley',
+  'edward',
+  'cole',
+  'addison',
+  'coleman',
+  'lucas',
+  'molly',
+  'tenzin',
+  'bennett',
+  'lydia',
+  'gemma',
+  'madison',
+  'keith',
+  'aubrey',
+  'strickland',
+  'mary',
+  'makenzie',
+  'chase',
+  'charles',
+  'eva',
+  'ingrid',
+  'robert',
+  'adair',
+  'bryce',
+  'henry',
+  'divyalakshmi',
+  'ellison',
+  'andrew',
+  'gaines',
+  'nicholas',
+  'landon',
+  'lucas',
+  'madeleine',
+  'ava',
+  'eden',
+  'treasure',
+  'merrick',
+  'xavier',
+  'madeline',
+  'jude',
+  'garrett',
+  'richard',
+  'joseph',
+  'whitney',
+  'william',
+  'bailey',
+  'thomas',
+  'bree',
+  'james',
+  'mary-grace',
+  'miller',
+]
+
+if (grade == null) {
+  var GRADEWORDS = nogradeyet
+} else if (grade == '"69"') {
+  console.log('nah thats crazy ')
+} else if (grade == '"25"') {
+  var GRADEWORDS = EIGHTGRADE
+} else if (grade == '"7"') {
+  var GRADEWORDS = SEVENGRADE
+} else if (grade == '"8"') {
+  var GRADEWORDS = EIGHTGRADE
+} else {
+  var GRADEWORDS = nogradeyet
+}
+export const WORDS = GRADEWORDS
