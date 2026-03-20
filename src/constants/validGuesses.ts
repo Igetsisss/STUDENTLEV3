dfe7780d2fa1c97b3e326a5c4b78d36d04c36539
Igetsisss <98964802+Igetsisss@users.noreply.github.com>
2026-03-20 11:30:41 -0400

    Refactor GRADEWORDS variable and update logic
    Refactor GRADEWORDS variable to use let instead of var and update logic to prevent overwriting grades. Adjusted conditions for setting GRADEWORDS based on grade values.

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -8,146 +8,68 @@ const gradeStatKey = 'gradeNumber'
 const grade = localStorage.getItem(gradeStatKey)
 const nogradeyet = ['jack']
 
-var GRADEWORDS = nogradeyet
+let GRADEWORDS = nogradeyet
+
 const EIGHTGRADE = [
-  "avery", "ella", "joselyn", "grant", "havana", "sean", "william", "mitchell", "hannah", "cole", "ryan", "hunter", "hayes", "hailey", "scarlett", "blair", "nicole", "moses", "emma", "amelia", "annabelle", "brad", "emerson", "danny", "andrew", "robert", "sidney-ann", "mcvey", "lillian", "caroline", "aj", "jack", "cesar", "amara", "ray", "ansley", "sophia", "leah", "katherine", "nakhai", "sienna", "luke", "ria", "caroline", "evelyn", "avalina", "tanner", "gavin", "kaitlynne", "will", "chase", "collier", "franklin", "bobby", "hotoniel", "daniel", "sam", "tuck", "emmy", "finn", "jones", "juliana", "maxx", "lilliana", "sophia", "claire", "jules", "meriwether", "elizabeth", "andrew", "layla", "greg", "griffin", "eliana", "emma", "alexa", "ryder", "marley", "bryce", "tyson", "audrey", "brent", "sasha", "jackson", "bralyn", "brooke", "caroline", "eva", "mary", "emma", "felipe", "patrick", "gabriella", "dean", "sarah", "tommy", "margot", "garrett", "margaret", "madeline", "preston", "phillip", "rhodes", "robby", "charlotte", "beatrice", "diego", "christina", "jackson", "eli", "amaree", "george", "kenzo", "tyler", "logan", "nora", "grace", "emma", "brendan", "grayson", "greyson", "katlynn", "madeline", "christian", "davis", "kade", "colt", "claire", "bryce", "matthew", "braiden", "william", "chloe", "teagan", "jack", "betsy", "connor", "jon", "ella", "olivia", "nick", "makayla", "harley", "paige", "grant", "sammy", "grace", "carter", "graham", "jack", "ryder"
+  'emerson', 'william', 'mitchell', 'rhodes', 'daniel', 'brooke', 
+  'avery', 'harris', 'grant', 'madeline', 'cesar', 'gavin', 
+  'davis', 'margot', 'hotoniel', 'jack', 'sam', 'robby', 
+  'margaret', 'gigi', 'christina', 'emmy', 'felipe', 'florencia', 
+  'grey', 'claire', 'caroline', 'hailey', 'ryan', 'jackson', 'aj', 
+  'amara', 'jon', 'anaya', 'patrick', 'juliana', 'nicholas', 
+  'finn', 'connor', 'andrew', 'ella', 'emma', 'kate', 'mary', 
+  'brendan', 'joselyn', 'mateo', 'eliana', 'graham', 'nora', 
+  'will', 'tuck', 'amaree', 'phillip', 'maxx', 'betsy', 
+  'sophie', 'cole', 'grant', 'alexa', 'sammy', 'sidney-ann', 
+  'blair', 'evelyn', 'gracie', 'ayokunle', 'meriwether', 
+  'paige', 'ria', 'amelia', 'hannah', 'katlynn', 'teagan', 
+  'beatrice', 'sarah', 'vance', 'hunter', 'brad', 'tyler', 
+  'greg', 'franklin', 'braiden', 'annabelle', 'preston', 
+  'maddie', 'tommy', 'collier', 'colt', 'dean', 'havana', 
+  'matthew', 'lily', 'kade', 'griffin', 'hayes', 'mose', 
+  'vandy', 'logan', 'scarlett', 'mcvey', 'george', 'chloe', 
+  'luke', 'layla', 'ryder', 'ray', 'victoria', 'mayomi', 
+  'maya', 'bryce', 'sienna', 'bobby', 'charlie', 'lilah', 
+  'sean', 'kenzo', 'leah', 'jules', 'gabriella', 'charlotte', 
+  'sasha', 'sophia', 'tanner', 'marley', 'eva', 'brent', 
+  'chase', 'harley', 'makayla', 'grayson', 'claire', 
+  'christian', 'munchie', 'nicole'
 ];
 
 const SEVENGRADE = [
-  'andrew',
-  'madison',
-  'tristen',
-  'brayden',
-  'xavier',
-  'jake',
-  'joseph',
-  'madeline',
-  'mary',
-  'strickland',
-  'ingrid',
-  'jackson',
-  'zoe',
-  'bennett',
-  'nicholas',
-  'william',
-  'cole',
-  'bailey',
-  'tenzin',
-  'keith',
-  'ronan',
-  'lucas',
-  'eden',
-  'bree',
-  'eva',
-  'vivienne',
-  'liam',
-  'jack',
-  'lane',
-  'hudson',
-  'thomas',
-  'perry',
-  'ryan',
-  'edward',
-  'garrett',
-  'richard',
-  'gaines',
-  'wyatt',
-  'emma',
-  'bryce',
-  'henry',
-  'grayden',
-  'emily',
-  'samuel',
-  'william',
-  'laura',
-  'elizabeth',
-  'lydia',
-  'davis',
-  'jay',
-  'samantha',
-  'isabella',
-  'sloane',
-  'violet',
-  'sara',
-  'cathryn-rose',
-  'mary-grace',
-  'cristiano',
-  'whitney',
-  'addison',
-  'ellison',
-  'charles',
-  'isabel',
-  'peighton',
-  'isabell',
-  'mary',
-  'julia',
-  'ryan',
-  'crosby',
-  'gemma',
-  'sophia',
-  'daniel',
-  'hugh',
-  'alexa',
-  'merrick',
-  'landon',
-  'connor',
-  'robert',
-  'ayden',
-  'emerson',
-  'caroline',
-  'lucas',
-  'lucy',
-  'turner',
-  'joshua',
-  'coleman',
-  'chase',
-  'ava',
-  'prakhar',
-  'mia',
-  'sovereign',
-  'riley',
-  'daniel',
-  'james',
-  'cole',
-  'jude',
-  'sarah',
-  'madeleine',
-  'molly',
-  'shane',
-  'miller',
-  'presley',
-  'lucas',
-  'aubrey',
-  'caroline',
-  'gailyn',
-  'logan',
-  'makenzie',
-  'william',
-  'adair',
-  'athena',
-  'treasure',
-  'sam',
-  'colton',
-  'patrick',
-  'ryan',
+  'andrew', 'madison', 'tristen', 'brayden', 'xavier', 'jake', 
+  'joseph', 'madeline', 'mary', 'strickland', 'ingrid', 'jackson', 
+  'zoe', 'bennett', 'nicholas', 'william', 'cole', 'bailey', 
+  'tenzin', 'keith', 'ronan', 'lucas', 'eden', 'bree', 'eva', 
+  'vivienne', 'liam', 'jack', 'lane', 'hudson', 'thomas', 
+  'perry', 'ryan', 'edward', 'garrett', 'richard', 'gaines', 
+  'wyatt', 'emma', 'bryce', 'henry', 'grayden', 'emily', 
+  'samuel', 'william', 'laura', 'elizabeth', 'lydia', 'davis', 
+  'jay', 'samantha', 'isabella', 'sloane', 'violet', 'sara', 
+  'cathryn-rose', 'mary-grace', 'cristiano', 'whitney', 'addison', 
+  'ellison', 'charles', 'isabel', 'peighton', 'isabell', 'mary', 
+  'julia', 'ryan', 'crosby', 'gemma', 'sophia', 'daniel', 'hugh', 
+  'alexa', 'merrick', 'landon', 'connor', 'robert', 'ayden', 
+  'emerson', 'caroline', 'lucas', 'lucy', 'turner', 'joshua', 
+  'coleman', 'chase', 'ava', 'prakhar', 'mia', 'sovereign', 
+  'riley', 'daniel', 'james', 'cole', 'jude', 'sarah', 'madeleine', 
+  'molly', 'shane', 'miller', 'presley', 'lucas', 'aubrey', 
+  'caroline', 'gailyn', 'logan', 'makenzie', 'william', 'adair', 
+  'athena', 'treasure', 'sam', 'colton', 'patrick', 'ryan', 
   'divyalakshmi',
 ]
 
+// Updated logic using 'else if' to prevent overwriting correct grades
 if (grade == null) {
-  var GRADEWORDS = nogradeyet
-}
-if (grade == '"69"') {
+  GRADEWORDS = nogradeyet
+} else if (grade == '"69"') {
   console.log('nah thats crazy ')
-}
-if (grade == '"25"') {
-  var GRADEWORDS = EIGHTGRADE
-}
-if (grade == '"7"') {
-  var GRADEWORDS = SEVENGRADE
-}
-if (grade == '"8"') {
-  var GRADEWORDS = EIGHTGRADE
+} else if (grade == '"25"' || grade == '"8"') {
+  GRADEWORDS = EIGHTGRADE
+} else if (grade == '"7"') {
+  GRADEWORDS = SEVENGRADE
 } else {
-  var GRADEWORDS = nogradeyet
+  GRADEWORDS = nogradeyet
 }
+
 export const VALID_GUESSES = GRADEWORDS
