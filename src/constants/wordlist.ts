03ff46b252163107399f0f1e042cdfab31597fff
Igetsisss <98964802+Igetsisss@users.noreply.github.com>
2026-04-09 09:11:27 -0400

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -12,12 +12,12 @@ let GRADEWORDS = nogradeyet
 
 const SENIOR = [
   'chase', 'ryan', 'rania', 'matthew', 'jb', 'henry', 'claire', 'marshall', 
-  'addie', 'arden', 'sophie', 'james', 'blair', 'kennedy', 'alex', 'ben', 
+  'addie', 'arden', 'sophie', 'james', 'blair', 'kennedy', 'alex', 'sairis', 
   'charlie', 'ollie', 'callie', 'beckett', 'vivian', 'gavin', 'sarah', 'mac', 
   'taylor', 'ethan', 'kate', 'ella', 'peyton', 'jack', 'grant', 'gabe', 'lily', 
   'brady', 'devin', 'sam', 'myles', 'carson', 'fallon', 'baxter', 'mary frances', 
   'maddie', 'grace', 'julie', 'lila', 'jordan', 'parker', 'jay', 'andrew', 
-  'caroline', 'nate', 'haylen', 'mary kathryn', 'sophia', 'marcos', 'sairis', 
+  'caroline', 'nate', 'haylen', 'mary kathryn', 'sophia', 'marcos', 'ben', 
   'alexa', 'zari', 'evie', 'jada', 'olivia', 'katherine', 'keval', 'jordynn', 
   'lewis', 'tommy', 'steele', 'dallas', 'austin', 'lilly', 'zion', 'rio', 
   'will', 'evan', 'cate', 'xavier', 'miller', 'anna', 'fletcher', 'khalid'
