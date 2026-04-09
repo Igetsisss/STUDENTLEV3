0ddd50d4562b61e29b90e4d3722b0770a6f301fa
Igetsisss <underwoodjack185@gmail.com>
2026-04-09 16:11:01 -0400

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = 'Studentle V4'
+export const GAME_TITLE = 'Studentle'
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
