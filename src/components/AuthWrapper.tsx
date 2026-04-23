c41a08d21da35998d724becf4058e1756f269c22
Igetsisss <underwoodjack185@gmail.com>
2026-04-23 13:59:16 -0400

    fix: add missing useRef import in AuthWrapper (caused white screen)

--- src/components/AuthWrapper.tsx
+++ src/components/AuthWrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { lookupAccountByEmail } from '../lib/api'
 import { isSchoolEmail, signOutMicrosoft as signOut } from '../lib/auth'
